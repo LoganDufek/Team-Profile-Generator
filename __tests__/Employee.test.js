fce9abc918fd504c79f11b16fea726e4b68fc818
Logan D <logandufek@gmail.com>
2021-07-25 01:40:28 -0400

    Employee.js and Manager.js and respective tests updated with logic. Currently all tests are passing

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,39 @@
+const Employee = require('../lib/Employee.js')
+
+
+test('creates an employee object', () => {
+    const employee = new Employee("Logan",2,"logandufek@gmail.com")
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String))
+
+});
+
+test('checks that the name from Employee object has been sucessfully chosen', () => {
+    const employee = new Employee("Logan", 2,"logandufek@gmail.com")
+
+    expect(employee.getName()).toEqual(employee.name);
+
+});
+
+test('checks that the ID from Employee object has been sucessfully chosen', () => {
+    const employee = new Employee("Logan", 2,"logandufek@gmail.com")
+
+    expect(employee.getID()).toEqual(employee.id);
+
+});
+
+test('checks that the Email from Employee object has been sucessfully chosen', () => {
+    const employee = new Employee("Logan", 2,"logandufek@gmail.com")
+
+    expect(employee.getEmail()).toEqual(employee.email);
+
+});
+
+test('checks that the Employee has been sucessfully returned', () => {
+    const employee = new Employee("Logan", 2,"logandufek@gmail.com")
+
+    expect(employee.getRole()).toEqual(Employee);
+
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,47 @@
-//Test
+
+const Manager = require('../lib/Manager.js')
+
+test('creates a Manager object', () => {
+    const manager = new Manager("Logan", 2,"logandufek@gmail.com", 5)
+
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+
+});
+
+test('checks that the name from Manager object has been sucessfully chosen', () => {
+    const manager = new Manager("Logan", 2,"logandufek@gmail.com", 5)
+
+    expect(manager.getName()).toEqual(manager.name);
+
+});
+
+test('checks that the ID from Manager object has been sucessfully chosen', () => {
+    const manager = new Manager("Logan", 2,"logandufek@gmail.com", 5)
+
+    expect(manager.getID()).toEqual(manager.id);
+
+});
+
+test('checks that the Email from Manager object has been sucessfully chosen', () => {
+    const manager = new Manager("Logan", 2,"logandufek@gmail.com", 5)
+
+    expect(manager.getEmail()).toEqual(manager.email);
+
+});
+
+test('checks that the ID from Manager object has been sucessfully chosen', () => {
+    const manager = new Manager("Logan", 2,"logandufek@gmail.com", 5)
+
+    expect(manager.getOfficeNumber()).toEqual(manager.officeNumber);
+
+});
+
+test('checks that the Manager has been sucessfully returned', () => {
+    const manager = new Manager("Logan", 2,"logandufek@gmail.com", 5)
+
+    expect(manager.getRole()).toEqual(Manager);
+
+});
