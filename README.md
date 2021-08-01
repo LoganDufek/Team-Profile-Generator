d5c42cce9cb47266b83fa421cf528766c72237eb
Logan D <logandufek@gmail.com>
2021-07-31 23:23:53 -0400

    README Updated and minor formatting changes

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 ## Challenge Ten Team-Profile-Generator
 
 ### Description
+The challenge this week was to take our Object Oriented Programming knowledge and pair it with what we learned from using inquirer the previous week. This involved creating several Classes such as Employee, Manager, Engineer and Intern. Likewise, unit tests were written for each of these files to ensure they first failed and then functioned as intended. In the end, these elements culminated in a functioning Team Profile generator.
 
 ### Screenshots
 
+In order to check that the tests suceed, invoke the jest script as follows
+![nmpRunTest](./img/npmRunTest.png)
+
+You should see something like this if the tests were sucessfull 
+![testSucess](./img/TestPass.png)
+
+Next to invoke the program, type the following and you'll be presented with the Manager inquirer prompts
+![nodeIndex](./img/nodeIndex.png)
+
+Following that, you'll be asked if you would like to add an Engineer or Intern, and fill out simmilar questions for them until you select Finish.
+![TeamMembers](./img/addTeamMembers.png)
+
+Finally, when you select finish, your file will be generated and you and you should see this message
+![HTML](./img/buildHTML.png)
+
 ### Links
 Github: [Link](https://github.com/LoganDufek/Team-Profile-Generator)
+YouTube: [Link](https://www.youtube.com/watch?v=vL-9BO4asFA)
--- index.js
+++ index.js
@@ -8,210 +8,257 @@ const Engineer = require('./lib/Engineer.js');
 
 //Overall class which is callled at the end in conjunction with promptUser to start the program
 class Team {
-    constructor(){
+    constructor() {
         //constructor function with an array for the created employees to later be added to
         this.allEmployees = [];
-        
-        
-}
-//Intro inquirer function for the Manager to fill out
-promptUser() {
 
-    inquirer
-    .prompt([{
-        type: "text",
-        name: 'managerName',
-        message: "What is the Manager's name?"
-    },
-    {
-        type: 'number',
-        name: 'ID',
-        message: "What is their ID number?",
-    },
-    {
-        type: 'input',
-        name: 'managerEmail',
-        message: "What is their email?"  
-    },
-    {
-        type: 'number',
-        name: 'officeNumber',
-        message: "What is their office number?"  
-    },
+    }
+    //Intro inquirer function for the Manager to fill out
+    promptUser() {
 
-    ])
-    //Take and descructure the answers given
-    .then(({ managerName, ID, managerEmail, officeNumber}) => {
-        // Use the required Manager class defined in another js file and assign those elements to a new variable
-       const manager = new Manager(managerName, ID, managerEmail, officeNumber);
+        inquirer
+            .prompt([{
+                type: "text",
+                name: 'managerName',
+                message: "What is the Manager's name?"
+            },
+            {
+                type: 'number',
+                name: 'ID',
+                message: "What is their ID number?",
+            },
+            {
+                type: 'input',
+                name: 'managerEmail',
+                message: "What is their email?"
+            },
+            {
+                type: 'number',
+                name: 'officeNumber',
+                message: "What is their office number?"
+            },
 
-       //HTML for the Manager elements that're assigned to a variable
-       let buildManager = `<div class="card" style="width: 18rem;">
-       <div class="card-header manager-title" ><h4> ${manager.name}</h4>
-       <h5> Manager </h5> </div>
-       <ul class="list-group list-group-flush">
-       <li class="list-group-item"><p> ID: ${manager.id} </p></li>
-       <li class="list-group-item"><p> Email: <a href="mailto:${manager.email}">${manager.email}</a>
-       </p></li>
-       
-       <li class="list-group-item"> <p> Office Number: ${manager.officeNumber} </p></li>
-       </ul>
-       </div>`
-       //The buildManager variable is then pushed into the exisitng allEmployees array
-       this.allEmployees.push(buildManager)
-   
-       console.log(manager)
+            ])
+            //Take and descructure the answers given
+            .then(({ managerName, ID, managerEmail, officeNumber }) => {
+                // Use the required Manager class defined in another js file and assign those elements to a new variable
+                const manager = new Manager(managerName, ID, managerEmail, officeNumber);
 
-      })
-      .then(() => {
-        //The Manager is then prompted by the addTeamMember function to either add an Engineer, Intern, or finish the function 
-        this.addTeamMember();
-      })
-      
+                //HTML for the Manager elements that're assigned to a variable
+                let buildManager = `<div class="row">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header manager-title">
+                        <h4>${manager.name}</h4>
+                            <h5> 
+                                Manager 
+                            </h5> 
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <p>
+                             ID: ${manager.id}
+                            </p>
+                        </li>
+                    <li class="list-group-item">
+                            <p> 
+                            Email: 
+                            <a href="mailto:${manager.email}">${manager.email}</a>
+                            </p>
+                    </li>
+                    <li class="list-group-item">
+                            <p> 
+                            Office Number: ${manager.officeNumber} 
+                            </p><
+                     /li>
+                    </ul>
+                </div>`
+                //The buildManager variable is then pushed into the exisitng allEmployees array
+                this.allEmployees.push(buildManager)
 
-}
-//Engineer Question List Function
-engineerQuestions() {
-inquirer
-        .prompt([{
-            type: 'text',
-            name: 'engineerName',
-            message: "What is the Engineer's name?"
-        },
-        {
-            type: 'number',
-            name: 'ID',
-            message: "What is their ID number?" 
-        },
-        {
-            type: 'text',
-            name: 'engineerEmail',
-            message: "What is their email?"  
-        },
-        {
-            type: 'text',
-            name: 'github',
-            message: "What is their GitHub username?"  
-        }
-    
-        ])
-        .then(({ engineerName, ID, engineerEmail, github}) => {
-             // Use the required Engineer class defined in another js file and assign those elements to a new variable
-             const engineer = new Engineer(engineerName, ID, engineerEmail, github);
+                console.log(manager)
+
+            })
+            .then(() => {
+                //The Manager is then prompted by the addTeamMember function to either add an Engineer, Intern, or finish the function 
+                this.addTeamMember();
+            })
+
+
+    }
+    //Engineer Question List Function
+    engineerQuestions() {
+        inquirer
+            .prompt([{
+                type: 'text',
+                name: 'engineerName',
+                message: "What is the Engineer's name?"
+            },
+            {
+                type: 'number',
+                name: 'ID',
+                message: "What is their ID number?"
+            },
+            {
+                type: 'text',
+                name: 'engineerEmail',
+                message: "What is their email?"
+            },
+            {
+                type: 'text',
+                name: 'github',
+                message: "What is their GitHub username?"
+            }
+
+            ])
+            .then(({ engineerName, ID, engineerEmail, github }) => {
+                // Use the required Engineer class defined in another js file and assign those elements to a new variable
+                const engineer = new Engineer(engineerName, ID, engineerEmail, github);
+
+                //HTML for the Engineer elements that're assigned to a variable
+                let buildEngineers = `<div class="row">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header engineer-title">
+                        <h4>${engineer.name}</h4>
+                            <h5> 
+                                Engineer 
+                            </h5> 
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <p>
+                             ID: ${engineer.id}
+                            </p>
+                        </li>
+                    <li class="list-group-item">
+                            <p> 
+                            Email: <a href="mailto:${engineer.email}">${engineer.email}</a>
+                            </p>    
+                    </li>
             
-            //HTML for the Engineer elements that're assigned to a variable
-            let buildEngineers = `<div class="card" style="width: 18rem;">
-            <div class="card-header engineer-title"> <h4> ${engineer.name}</h4>
-            <h5> Engineer </h5> </div>
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item"><p> ID: ${engineer.id} </p></li>
-            <li class="list-group-item"><p> Email: <a href="mailto:${engineer.email}">${engineer.email}</a>
-            </p></li>
-            
-            <li class="list-group-item"> <p>  GitHub: <a href="https://github.com/${engineer.github}"> ${engineer.github}</a></p></li>
-            </ul>
-            </div>`
-             //The buildEngineer variable is then pushed into the exisitng allEmployees array
-            this.allEmployees.push(buildEngineers)
-            console.log(engineer)
-    
-          })
-          .then(() => {
-             //The Manager is then prompted by the addTeamMember function to either add an Engineer, Intern, or finish the function 
-            this.addTeamMember();
-          })
-        }
-//Intern Question List Function
-internQuestions(){
-    inquirer
-    .prompt([{
-        type: 'text',
-        name: 'internName',
-        message: "What is the Intern's name?"
-    },
-    {
-        type: 'number',
-        name: 'ID',
-        message: "What is their ID number?" 
-    },
-    {
-        type: 'text',
-        name: 'internEmail',
-        message: "What is their email?"  
-    },
-    {
-        type: 'text',
-        name: 'school',
-        message: "What University/College are they currently attending?"  
+                    <li class="list-group-item"> 
+                            <p>  
+                            GitHub: <a href="https://github.com/${engineer.github}"> ${engineer.github}</a>
+                            </p>
+                    </li>
+                    </ul>
+                </div>`
+                //The buildEngineer variable is then pushed into the exisitng allEmployees array
+                this.allEmployees.push(buildEngineers)
+                console.log(engineer)
+
+            })
+            .then(() => {
+                //The Manager is then prompted by the addTeamMember function to either add an Engineer, Intern, or finish the function 
+                this.addTeamMember();
+            })
+    }
+    //Intern Question List Function
+    internQuestions() {
+        inquirer
+            .prompt([{
+                type: 'text',
+                name: 'internName',
+                message: "What is the Intern's name?"
+            },
+            {
+                type: 'number',
+                name: 'ID',
+                message: "What is their ID number?"
+            },
+            {
+                type: 'text',
+                name: 'internEmail',
+                message: "What is their email?"
+            },
+            {
+                type: 'text',
+                name: 'school',
+                message: "What University/College are they currently attending?"
+            }
+
+            ])
+            .then(({ internName, ID, internEmail, school }) => {
+                // Use the required Intern class defined in another js file and assign those elements to a new variable
+                const intern = new Intern(internName, ID, internEmail, school);
+
+                //HTML for the Intern elements that're assigned to a variable
+                let buildIntern = `<div class="row">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header intern-title">
+                        <h4>${intern.name}</h4>
+                            <h5> 
+                                Intern 
+                            </h5> 
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <p>
+                             ID: ${intern.id}
+                            </p>
+                        </li>
+                    <li class="list-group-item">
+                            <p> 
+                            Email: 
+                            <a href="mailto:${intern.email}">${intern.email}</a>
+                            </p>
+                    </li>
+                    <li class="list-group-item">
+                            <p> 
+                            School: ${intern.school} 
+                            </p><
+                     /li>
+                    </ul>
+                </div>`
+                //The buildIntern variable is then pushed into the exisitng allEmployees array
+                this.allEmployees.push(buildIntern)
+
+                console.log(intern)
+
+            })
+            .then(() => {
+                //The Manager is then prompted by the addTeamMember function to either add an Engineer, Intern, or finish the function 
+                this.addTeamMember();
+            })
+
+    }
+    //add Team Member function that is called after every election
+    addTeamMember() {
+        inquirer
+            .prompt([{
+                type: 'list',
+                name: 'addTeamMembers',
+                message: 'Would you like to add a Team Member or Finish Building your Team Profile?',
+                choices: ['Add Engineer', 'Add Intern', 'Finish']
+            }])
+            //addTeamMembers is destructured and then a different function is called depending on the answer 
+            .then(({ addTeamMembers }) => {
+                if (addTeamMembers === "Add Engineer") {
+                    console.log("Engineer Selected")
+
+                    this.engineerQuestions();
+                }
+                else if (addTeamMembers === "Add Intern") {
+                    console.log("Intern Selected")
+
+                    this.internQuestions();
+                }
+                else if (addTeamMembers === "Finish") {
+                    console.log("Building Page!")
+
+                    this.buildHTML();
+                }
+            })
+
     }
 
-    ])
-    .then(({ internName, ID, internEmail, school}) => {
-        // Use the required Intern class defined in another js file and assign those elements to a new variable
-         const intern = new Intern(internName, ID, internEmail, school);
+    //Function to generate the HTML once all the employees have been added
+    buildHTML() {
 
-        //HTML for the Intern elements that're assigned to a variable
-        let buildIntern = `<div class="card" style="width: 18rem;">
-        <div class="card-header intern-title" ><h4> ${intern.name}</h4>
-        <h5> Intern </h5> </div>
-        <ul class="list-group list-group-flush">
-        <li class="list-group-item"><p> ID: ${intern.id} </p></li>
-        <li class="list-group-item"><p> Email: <a href="mailto:${intern.email}">${intern.email}</a>
-        </p></li>
-        
-        <li class="list-group-item"> <p> School: ${intern.school} </p></li>
-        </ul>
-        </div>`
-        //The buildIntern variable is then pushed into the exisitng allEmployees array
-        this.allEmployees.push(buildIntern)
-       
-        console.log(intern)
+        //allEmployees array are then formatted appropriately so they appear as their own entity on the page
+        const organizedArray = this.allEmployees.join(' ');
 
-      })
-      .then(() => {
-         //The Manager is then prompted by the addTeamMember function to either add an Engineer, Intern, or finish the function 
-        this.addTeamMember();
-      })
-
-}
-//add Team Member function that is called after every election
-addTeamMember(){
-    inquirer
-        .prompt([{
-        type: 'list',
-        name: 'addTeamMembers',
-        message: 'Would you like to add a Team Member or FinisBuilding your Team Profile?',
-        choices: ['Add Engineer', 'Add Intern', 'Finish']
-    }])
-    //addTeamMembers is destructured and then a different function is called depending on the answer 
-    .then(({addTeamMembers}) => {
-        if (addTeamMembers === "Add Engineer"){
-            console.log("Engineer Selected")
-    
-            this.engineerQuestions();
-                }
-        else if (addTeamMembers === "Add Intern"){
-            console.log("Intern Selected")
-
-            this.internQuestions();
-        }
-        else if (addTeamMembers === "Finish"){
-            console.log("Building Page!")
-
-            this.buildHTML();
-        }
-    })
-    
-}     
-
-//Function to generate the HTML once all the employees have been added
-buildHTML(){
-
-//allEmployees array are then formatted appropriately so they appear as their own entity on the page
- const organizedArray = this.allEmployees.join(' ');
-    
-    var employeeData =  
-        `<!DOCTYPE html>
+        var employeeData =
+            `<!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
@@ -242,9 +289,9 @@ buildHTML(){
             }
             console.log('File created!')
         })
-    
 
-}
+
+    }
 
 }
 
